a7779dff2bd77b84a4ec28282ca75a277e1d3df7
Melih Kaan Yıldız <melihkaanyildiz@gmail.com>
2023-07-13 12:04:09 +0300

    Create hash_http_response.py

---
+++ hash_http_response.py
@@ -0,0 +1,70 @@
+import requests
+import sys
+import hashlib
+
+VERBOSE = True
+VERBOSE = False
+
+def hash_response(url, level=2):
+    """
+    This function will hash the http response of the given url
+    The way this function will work is 
+    
+    sha256(response.status_code + response.reason +  response.headers + response.body )
+    """
+
+    try:
+        response = requests.get(url, allow_redirects=False, timeout=4, verify=False)
+
+        body = response.text
+        headers = ""
+        for header in response.headers:
+            headers += header + ": " + response.headers[header] + "\r\n"
+        status_code = str(response.status_code)
+        reason = response.reason
+
+        if VERBOSE:
+            print(f"{status_code} {reason}")
+            print(headers)
+            print(body)
+
+        # level 1 = body 
+        # level 2 = body + status_code + reason
+        # level 3 = body + headers + status_code + reason
+
+
+        if level == 1:
+            raw_response = body
+        elif level == 2:
+            raw_response = body + status_code + reason
+        elif level == 3:
+            raw_response = status_code + reason + body + headers
+
+        hash = hashlib.sha256(raw_response.encode()).hexdigest()
+        return hash
+    
+    except Exception:
+        return None
+
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        level = int(sys.argv[2])
+    else:
+        level = 2
+    # check if url has schema in it (http/https)
+    if "http" not in url:
+        # just check http for now
+        url = "http://" + url
+    
+    # compare levels
+    # level 1 = body 
+    # level 2 = body + status_code + reason
+    # level 3 = body + headers + status_code + reason
+    # level 2 is default
+
+    hash = hash_response(url, level)
+    print(hash)
